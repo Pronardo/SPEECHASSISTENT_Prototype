7cd88e48dd1cc17692ac253538b04ff237162fc7
Pronardo <projetekt@outlook.de>
2018-10-11 13:52:18 +0200

    Initial Commit

---
+++ AAA_Speech_Proto/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AAA_Speech_Proto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SpeechRecon speechRecon = new SpeechRecon();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRecognize_Click(object sender, RoutedEventArgs e)
+        {
+            speechRecon.StartRecognize(SpeechRecognized);
+        }
+
+        private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            lblText.Content = e.Result.Text;
+        }
+    }
+}
---
+++ AAA_Speech_Proto/SpeechRecon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Globalization;
+
+namespace AAA_Speech_Proto
+{
+    class SpeechRecon
+    {
+        public void StartRecognize(EventHandler<SpeechRecognizedEventArgs> speechRecognized)
+        {
+
+            using (var recognizer = new SpeechRecognitionEngine(new CultureInfo("en-US")))
+            {
+                recognizer.LoadGrammar(new DictationGrammar());
+                recognizer.SpeechRecognized += speechRecognized;
+                recognizer.SetInputToDefaultAudioDevice();
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+
+    }
+}
