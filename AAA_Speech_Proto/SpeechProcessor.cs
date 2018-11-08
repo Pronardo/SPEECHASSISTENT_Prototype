32d66b0bec9b03529829a0ad64946e61ac722f84
Pronardo <projetekt@outlook.de>
2018-11-08 15:07:47 +0100

    Added SpeechProcessor

--- AAA_Speech_Proto/SpeechProcessor.cs
+++ AAA_Speech_Proto/SpeechProcessor.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace AAA_Speech_Proto
 {
-    interface SpeechProcessor
+    public abstract class SpeechProcessor
     {
-        void StartRecognize(string order);
+        protected bool isInitialized = false;
+        internal abstract void DoProcess(SpeechCommand command);
+
+        public void Process(string input)
+        {
+            if (!isInitialized) throw new InvalidOperationException("Method Init(string) not called!");
+            SpeechCommand command = ExtractCommand(input);
+            DoProcess(command);
+        }
+
+        private SpeechCommand ExtractCommand(string input)
+        {
+            Match m = Regex.Match(input, @"(?<control>\w+)\s+(?<property>\w+)\s+(?<value>\w+)");
+            SpeechCommand command=new SpeechCommand();
+            command.ControlName = m.Groups["control"].Value;
+            command.PropertyName = m.Groups["property"].Value;
+            command.Value = m.Groups["value"].Value;
+            return command;
+        }
+
+ 
     }
 }
--- AAA_Speech_Proto/WPFSpeechProcessor.cs
+++ AAA_Speech_Proto/WPFSpeechProcessor.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace AAA_Speech_Proto
 {
-    class WPFSpeechProcessor : SpeechProcessor
+    public class WPFSpeechProcessor : SpeechProcessor
     {
-        public void StartRecognize(string order)
+        protected List<UIElement> controls = new List<UIElement>();
+
+        //public WPFSpeechProcessor(Window window) : base(window) { }
+        public void Init(Window window)
         {
-            throw new NotImplementedException();
+            isInitialized = true;
+            var panel = window.Content as Panel;
+            if (panel == null) return;
+            FindControls(panel);
+            controls.ForEach(x => Console.WriteLine($"Found {x}"));
+        }
+
+        private void FindControls(Panel panel)
+        {
+            foreach (var control in panel.Children.OfType<UIElement>())
+            {
+                Console.WriteLine($"adding {control}");
+                if (control is Panel) FindControls(control as Panel);
+                else controls.Add(control);
+            }
+        }
+
+        internal override void DoProcess(SpeechCommand command) //public void process(string context, string input)
+        {
+
+            //Assembly assembly = Assembly.GetExecutingAssembly(); // Assembly mscorlib = typeof(string).Assembly;
+            //Type[] types = assembly.GetTypes();
+            //var windows = types.ToList()
+            //    .Where(current => current.BaseType == typeof(Window));
+            //var window = windows.First();
+            Console.WriteLine("");
+
+            //controls.Where(x.Tag => command.ControlName);
         }
     }
 }
