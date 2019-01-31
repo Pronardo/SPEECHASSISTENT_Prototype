8a6ec38662aaec5eb44d4ff6cdc65ca8271fe45b
Pronardo <projetekt@outlook.de>
2019-01-31 13:13:35 +0100

    Changes in Code Structure & Style

--- AAA_Speech_Proto/Text2Speech/WPFMicrosoftSynthesizer.cs
+++ AAA_Speech_Proto/Text2Speech/WPFMicrosoftSynthesizer.cs
@@ -15,15 +15,16 @@ using System.Windows.Threading;
 
 namespace AAA_Speech_Proto.Text2Speech
 {
-    class WPFMicrosoftSynthesizer : SpeechSynth
+    class WPFMicrosoftSynthesizer : SpeechSynth, IObserver<UIElement>
     {
+        private SpeechSynthesizer synthesizer;
         //Tasks:
         //Debounce to MouseMoveEvent
         //Compare e.Source to Mappings
         //Get Mapped Property of recieved element
         //Give String to TTS Engine
         //WPF Functionality must be outsourced to SynthProcessor resp. WPFSynthProcessor
-        public int DebounceTimer { get; set; } = 300; //milliseconds
+        public int DebounceTimer { get; set; } = 1000; //milliseconds
         private Dictionary<string, string> SpeechMappings = new Dictionary<string, string>();
         public UIElement ObservedElement { get; set; }
         public WPFMicrosoftSynthesizer(UIElement element)
@@ -33,9 +34,21 @@ namespace AAA_Speech_Proto.Text2Speech
         public void Init(UIElement element)
         {
             SeedMappings();
+            InitSynthesizer();
             ObserveMouse(element);
             ObservedElement = element;
         }
+
+        private void InitSynthesizer()
+        {
+            synthesizer = new SpeechSynthesizer
+            {
+                Volume = 100,
+                Rate = -2
+            };
+            synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0, CultureInfo.GetCultureInfo("en-US"));
+        }
+
         private void SeedMappings()
         {
             SpeechMappings.Add("Button", "Content");
@@ -44,35 +57,32 @@ namespace AAA_Speech_Proto.Text2Speech
         }
         public void SynthesizeInput(string input)
         {
-            if (String.IsNullOrEmpty(input)){ Console.WriteLine("Synthesize Failed input string null or empty");  return; }
+            if (String.IsNullOrEmpty(input)) { Console.WriteLine("Synthesize Failed input string null or empty"); return; }
             Console.WriteLine($"synthesize input {input}");
-            using (var synthesizer = new SpeechSynthesizer())
-            {
-                synthesizer.Volume = 100;
-                synthesizer.Rate = -2;
-                synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0, CultureInfo.GetCultureInfo("en-US"));
-                synthesizer.Speak(input);
-            }
+            //using (var synthesizer = new SpeechSynthesizer())
+            //{
+            //    synthesizer.Volume = 100;
+            //    synthesizer.Rate = -2;
+            //synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0, CultureInfo.GetCultureInfo("en-US"));
+            synthesizer.Speak(input);
+            //}
         }
 
         public void ObserveMouse(UIElement element)
         {
             var mouseMove = Observable
                 .FromEventPattern<MouseEventArgs>(element, "MouseMove")
-                .Select(x => x.EventArgs.Source)
+                .Select(x => x.EventArgs.Source as UIElement)
+                .Where(x => x != null)
                 .Sample(TimeSpan.FromMilliseconds(DebounceTimer))
-               .Subscribe(
-                    completed => Process(completed),
-                    error => LogError(error)
-            );
+            //   .Subscribe(
+            //        next => Process(next),
+            //        error => LogError(error)
+            //);
+            .Subscribe(this);
         }
 
-        private void LogError(Exception error)
-        {
-            Console.WriteLine("An error occured while processing", error.Message);
-        }
-
-        private void Process(object element)
+        public void OnNext(UIElement element)
         {
             Type type = element.GetType();
             string eletype = type.Name;
@@ -83,15 +93,15 @@ namespace AAA_Speech_Proto.Text2Speech
                 var prop = SpeechMappings[eletype];
 
 
-                Console.WriteLine($"Current UI Thread : {Thread.CurrentThread.Name} associated with {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"Current Synth Thread : {Thread.CurrentThread.Name} associated with {Thread.CurrentThread.ManagedThreadId}");
                 string propertyvalue = "";
                 Application.Current.Dispatcher.Invoke(
                     new Action(() =>
                     {
                         PropertyInfo property = type.GetProperty(prop);
-                        Console.WriteLine($"reflection to property: {property}");
+                        Console.WriteLine($"   reflection to property: {property}");
                         propertyvalue = property.GetValue(element).ToString();
-                        Console.WriteLine($"reflection to proeprty value: {propertyvalue}");
+                        Console.WriteLine($"   reflection to proeprty value: {propertyvalue}");
                     }));
 
                 //PropertyInfo property = type.GetProperty(prop);
@@ -102,8 +112,20 @@ namespace AAA_Speech_Proto.Text2Speech
             }
             else
             {
-                Console.WriteLine($"Element {eletype} is not mapped with speech settings");
+                Console.WriteLine($"-- Element {eletype} is not mapped with speech settings");
             }
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("***** An error occured while processing", error.Message);
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("==============================");
+            Console.WriteLine("MouseMove-Observable completed");
+            Console.WriteLine("==============================");
         }
     }
 }
