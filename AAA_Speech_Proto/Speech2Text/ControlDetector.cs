3b7da156b71c0611532df8d1998ba94467ca2192
Pronardo <>
2019-03-11 12:57:52 +0100

    Predefined WPFGrammarSet

---
+++ AAA_Speech_Proto/Speech2Text/ControlDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AAA_Speech_Proto.Speech2Text
+{
+    static class ControlDetector
+    {//List can be set, but does not affect operation
+        public static List<UIElement> Controls { get; set; } = new List<UIElement>();
+        public static List<UIElement> FindControls(Window window)
+        {
+            var panel = window.Content as Panel;
+            Controls = new List<UIElement>();
+            foreach (var control in panel.Children.OfType<FrameworkElement>())
+            {
+                Console.WriteLine($"   adding {control}");
+                if (control is Panel) FindControls(control as Window);
+                else Controls.Add(control);
+            }
+            return Controls;
+        }
+        public static void LogControls()
+        {
+            Console.WriteLine("ControlDector - current COntrols");
+            Controls.ForEach(x => Console.WriteLine($"  Found {x}"));
+        }
+    }
+}
--- AAA_Speech_Proto/Speech2Text/WPFGrammarSet.cs
+++ AAA_Speech_Proto/Speech2Text/WPFGrammarSet.cs
@@ -4,23 +4,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace AAA_Speech_Proto.Speech2Text
 {
     class WPFGrammarSet
     {
-        public WPFGrammarSet()
+        private Window currentWin;
+        public Grammar Commands { get; set; }
+        public void SetWindow(Window window)
         {
-            Choices ch_Tags = new Choices(); //Read in runtime?
+            currentWin = window;
+            CreateWindowGrammar();
+        }
+
+        private void CreateWindowGrammar()
+        {
+            Choices ch_Tags = new Choices();//Read in runtime?
             Choices ch_Properties = new Choices(); //Fixed Ones?
             Choices ch_Values = new Choices();//Any?
+            var controls=ControlDetector.FindControls(currentWin);
+            foreach (var control in controls)
+            {
+                var identified=control as FrameworkElement;
+                ch_Tags.Add($"{identified.Tag}");
+                var properties = control.GetType().GetProperties();
+                foreach (var property in properties)
+                {
+                    ch_Properties.Add($"{property.Name}");
+                }
+            }
+
+ 
             GrammarBuilder baseSets = new GrammarBuilder();
             baseSets.Append($"{ch_Tags} ");
             baseSets.Append($"{ch_Properties} ");
             baseSets.Append($"{ch_Values}");
-            Grammar commands = new Grammar(baseSets);
+            Commands = new Grammar(baseSets);
         }
-
-
     }
 }
